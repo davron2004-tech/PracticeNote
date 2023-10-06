f460ef5295c2cf6addc3ea814ead933e82f16765
asror <a.aliqulov@student.inha.uz>
2023-10-06 22:18:35 +0500

    Initial Commit

---
+++ PracticeNote/PracticeNoteApp.swift
@@ -0,0 +1,17 @@
+//
+//  PracticeNoteApp.swift
+//  PracticeNote
+//
+//  Created by Asror Aliqulov on 06/10/23.
+//
+
+import SwiftUI
+
+@main
+struct PracticeNoteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

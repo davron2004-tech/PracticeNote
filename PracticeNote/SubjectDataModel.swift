c82e50d583748b5c448d17b8582a52f65b8fc30a
Davron Abdukhakimov <davron0506150@gmail.com>
2023-10-07 18:27:27 +0500

    complete data model

--- PracticeNote/SubjectDataModel.swift
+++ PracticeNote/SubjectDataModel.swift
@@ -14,11 +14,57 @@ class SubjectDataModel:Identifiable{
     var id = UUID()
     var subjectName:String
     var emoji:String
-    var words:[String]?
-    var lessons:[String]?
+    var words:[String] = []
+    var lessons:[LessonDataModel] = []
     init(subjectName: String, emoji: String) {
         self.subjectName = subjectName
         self.emoji = emoji
        
+    }
+}
+
+@Model
+class FolderDataModel:Identifiable{
+    var id = UUID()
+    var folderName:String
+    var lessons:[LessonDataModel] = []
+    init(folderName: String) {
+        
+        self.folderName = folderName
+    }
+}
+
+@Model
+class LessonDataModel: Identifiable {
+    var id = UUID()
+    var lessonName:String
+    var notes:[NoteDataModel] = []
+    init(lessonName: String) {
+        
+        self.lessonName = lessonName
+        
+    }
+}
+@Model
+class NoteDataModel:Identifiable{
+    var id = UUID()
+    var text:String
+    var imageData:Data
+    var cards:[CardDataModel] = []
+    init(text: String, imageData: Data) {
+        
+        self.text = text
+        self.imageData = imageData
+    }
+}
+@Model
+class CardDataModel:Identifiable{
+    var id = UUID()
+    var face:String
+    var back:String
+    init(face: String, back: String) {
+        
+        self.face = face
+        self.back = back
     }
 }

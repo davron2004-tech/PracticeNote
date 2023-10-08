9bf756b9fb758aa1f806c8b81ea0763b96590e88
Davron Abdukhakimov <davron0506150@gmail.com>
2023-10-08 11:59:04 +0500

    folder view

---
+++ PracticeNote/LessonsView.swift
@@ -0,0 +1,18 @@
+//
+//  LessonsView.swift
+//  PracticeNote
+//
+//  Created by Davron Abdukhakimov on 08/10/23.
+//
+
+import SwiftUI
+
+struct LessonsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LessonsView()
+}
